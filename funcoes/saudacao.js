0fb20a517faf48ed4c46a3e3c1da53a617593098
Petrucii <marcoshenriquepetruci@hotmail.com>
2024-08-16 22:00:32 -0300

    update

---
+++ funcoes/saudacao.js
@@ -0,0 +1,17 @@
+
+    /*Como criar uma função?
+
+Para criar uma função, você usa a palavra-chave function, seguida pelo nome da função, parênteses e
+
+um bloco de código entre chaves { }.
+ */
+
+    function saudacao() {
+        let nome = prompt('Digite seu nome');
+        console.log('Olá Mundo' + nome);
+        document.write('<h1>Seja bem vindo!<br><br>' + nome + '</h1><br>');
+    
+    }
+
+    saudacao();
+
